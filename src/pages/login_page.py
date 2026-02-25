99aa8547e15baffb6452fb68fd7db759c2c48a27
srinivas <srinivasaraothata@gmail.com>
2026-02-24 18:59:43 -0600

    Add login page object

---
+++ src/pages/login_page.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from src.pages.base_page import BasePage
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+class LoginPage(BasePage):
+    # Locators
+    USERNAME = (By.NAME, "username")
+    PASSWORD = (By.NAME, "password")
+    LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
+    ERROR_MESSAGE = (By.XPATH, "//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")
+    DASHBOARD_HEADER = (By.XPATH, "//h6[text()='Dashboard']")
+
+    def open(self, url):
+        logger.info(f"Opening URL: {url}")
+        self.driver.get(url)
+
+    def login(self, username, password):
+        logger.info(f"Logging in with username: {username}")
+        self.type_text(self.USERNAME, username)
+        self.type_text(self.PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
+
+    def get_error_message(self):
+        return self.get_text(self.ERROR_MESSAGE)
+
+    def is_dashboard_displayed(self):
+        return self.is_displayed(self.DASHBOARD_HEADER)
